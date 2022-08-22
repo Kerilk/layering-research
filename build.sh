cd36b0fff5ffcdc22a8f13a3f45bca6796dac720
Brice Videau <bvideau@anl.gov>
2022-08-22 15:48:51 +0000

    Fixed some build commands.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-gcc -Wall -Wextra -pedantic -std=c99 -g test.c -o test -L./ -lexp-loader
-gcc -Wall -Wextra -pedantic -std=c99 -g -shared -DDRIVER_NUMBER=2 driver.c -o libdriver2.so
-gcc -Wall -Wextra -pedantic -std=c99 -g -shared driver.c -o libdriver1.so
-gcc -Wall -Wextra -pedantic -std=c99 -g -shared exp-loader.c -o libexp-loader.so -ldl
+gcc -Wall -Wextra -pedantic -std=c99 -fPIC -g -shared -DDRIVER_NUMBER=2 driver.c -o libdriver2.so
+gcc -Wall -Wextra -pedantic -std=c99 -fPIC -g -shared driver.c -o libdriver1.so
+gcc -Wall -Wextra -pedantic -std=c99 -fPIC -g -shared exp-loader.c -o libexp-loader.so -ldl -lpthread
+gcc -Wall -Wextra -pedantic -std=c99 -fPIC -g test.c -o test -L./ -lexp-loader
